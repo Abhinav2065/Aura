eacd59275750bcd79605d199a1eaa4f3764abc9f
Abhinav2065 <abhinavsl511@gmail.com>
2025-10-27 11:17:40 +0545

    First Commit

---
+++ frontend/src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    This is a test Message to see what shows up on the website
+  </StrictMode>,
+)
