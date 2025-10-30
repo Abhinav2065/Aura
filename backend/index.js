070677346aecccc2abeeb6a748623e9261dc2433
Abhinav2065 <abhinavsl511@gmail.com>
2025-10-30 21:00:29 +0545

    Added an user model

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,19 @@ const morgan = require("morgan");
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("Connected to MogoDB"))
+    .catch(err => console.log("There is an error:", err));
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+
+app.get("/", (req,res) => {
+    res.json({
+        message : "The Backend is working!"
+    });
+})
 
 
 app.listen(8800, () => {
